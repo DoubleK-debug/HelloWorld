e031fd86bbaed5e335c7ff736bc063934454bed7
Kaniel <Kaniel.kusi.2022@student.lhschools.org>
2020-02-24 12:14:15 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,16 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print ("My name is");
+        System.out.println(" Double K ");
+
+
+//Print three line about what you did last summer
+        System.out.println("I slept");
+        System.out.println("Played Video Games ");
+        System.out.println("And just chilled out ");
+    }
+}
